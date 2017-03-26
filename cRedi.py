8988a75f8d52a493ccd4f955b28a84a94c41b84f
MrValdez <shunyvaldez@gmail.com>
2017-03-26 13:42:42 +0800

    initial testing of opening Google Map API via geo

--- cRedi.py
+++ cRedi.py
@@ -1,4 +1,7 @@
+import motionless
 import redis
+import webbrowser
+
 redis_db = redis.StrictRedis()
 
 def load_testdata():
@@ -8,6 +11,14 @@ def load_testdata():
 def test_testdata():
     dist = redis_db.geodist("shops", "Palermo", "Catania", "km")
     print(dist)
-    
+
     shops = redis_db.georadius("shops", 15, 37, 1000, "km")
     print(shops)
+
+    url = motionless.CenterMap(lat=37.502669, lon=15.087269).generate_url()
+    webbrowser.open(url)
+
+    map = motionless.DecoratedMap(lat=37.502669, lon=15.087269)
+    map.add_marker(motionless.LatLonMarker(37.502669, 15.087269))
+    map.add_marker(motionless.LatLonMarker(13.361389, 38.115556))
+    webbrowser.open(map.generate_url())
